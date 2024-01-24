2a2f5d6275cf904277e4ed27fdbec133fea6c9bb
Thomas Kamperman <thomaskamperman@gmail.com>
2024-01-24 14:13:41 +0100

    Added file reader to the settings class

--- MainClasses/Main.java
+++ MainClasses/Main.java
@@ -2,10 +2,17 @@ package MainClasses;
 
 import ComputerVision.ObjectDetection;
 import  Hardware.Hardware;
+import Utils.Settings.*;
 
 public class Main {
 
     ObjectDetection ObjDet = new ObjectDetection(Hardware.cam1);
+    static SettingsReader settingsReader = new SettingsReader();
 
+    public static void main(String[] args) {
+        settingsReader.read();
+    }
+
+        
     
 }
--- Utils/Settings/SettingsReader.java
+++ Utils/Settings/SettingsReader.java
@@ -1,11 +1,45 @@
 package Utils.Settings;
 
-import java.io.IOException;
 import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException; 
 
 
 public class SettingsReader {
-    
-    File file = new File("Utils\\Settings\\settings.set");
+  
+  
+
+    public SettingsReader()
+    {
+
+    }
+
+    public String read()
+    {
+        String data = "Unkown";
+
+        try
+        {
+            File file = new File("Utils\\Settings\\settings.set");
+            Scanner reader = new Scanner(file);
+
+            while (reader.hasNextLine()) {
+                data = reader.nextLine();
+                System.out.println(data);
+
+            }
+            reader.close();
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        return data;
+    }
+   
+    public void parse()
+    {
+
+    }
 
 }
